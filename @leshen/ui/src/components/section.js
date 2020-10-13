47bfde25504fe469b168c23adbaf60f675cc3381
Cade Kynaston <cade.kynaston@Cades-MacBook-Pro.local>
2020-10-13 14:33:21 -0600

    added H1 to section

--- @leshen/ui/src/components/section.js
+++ @leshen/ui/src/components/section.js
@@ -4,6 +4,7 @@ import { jsx } from '@emotion/core'
 const Section = ({ children }) => {
   return (
     <div css={{backgroundColor: 'coral', color: 'white'}}>
+      <h1>@Leshen/ui Section</h1>
       {children}
     </div>
   )
