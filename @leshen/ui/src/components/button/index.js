66745ef7d8eb8da4be901c68589104f6ad9d6408
Cade Kynaston <cade.kynaston@Cades-MacBook-Pro.local>
2020-10-14 14:58:02 -0600

    fixed styling

--- @leshen/ui/src/components/button/index.js
+++ @leshen/ui/src/components/button/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
 const Button = ({ children, size }) => (
-  <button type="button">
-    {`${size} - ${children}`}
-  </button>
+  <StyledButton type="button">
+    {children}
+  </StyledButton>
 );
+
+const StyledButton = styled.button`
+  background-color: tomato;
+`;
 
 export default Button;
